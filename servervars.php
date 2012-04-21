97c1dae6ed15505c3e38abec53c1290f0216f1ca
Miguel Diaz <migueldiazestrada@gmail.com>
2012-04-20 20:12:40 -0500

    primer commit luego de agregar el php

---
+++ servervars.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Script para probar el contenido de las variables php de servidor
+ * 
+ * @author Miguel Díaz Estrada <dev@migueltonic.pe>
+ * @package tools
+ * @subpackage pruebas
+ */
+
+echo "HTTP_HOST<br/>".$_SERVER['HTTP_HOST'];
+
+echo "<hr/>";
+
+echo "REQUEST_URI<br/>".$_SERVER['REQUEST_URI'];
+
+echo "<hr/>";
+
+echo "QUERY_STRING<br/>".$_SERVER['QUERY_STRING'];
+
+echo "<hr/>";
+
+echo "THE_REQUEST<br/>".$_SERVER['THE_REQUEST']."GET /string-string-digits? HTTP/1.1";
+
+echo "<hr/>";
+
+echo "REQUEST_FILENAME<br/>".$_SERVER['REQUEST_FILENAME'];
+
+echo "<hr/>";
+
+$CCL_HOST = ( substr_count($_SERVER['HTTP_HOST'],".")==2 )? substr($_SERVER['HTTP_HOST'],strpos($_SERVER['HTTP_HOST'],".")+1) : $_SERVER['HTTP_HOST'];
+
+echo $CCL_HOST;
+
+
+?>
